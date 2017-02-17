0bcf75884b7a4026a11ab5456fe9eb01e14aab9b
kribe48 <kribe606@gmail.com>
2017-02-17 09:19:48 +0100

    Correct naming convention

--- src/PiggyMcPigface.java
+++ src/PiggyMcPigface.java
@@ -1,13 +1,23 @@
 //Version
 import java.util.Scanner;
 
-class piggyMcPigface {
+class PiggyMcPigface {
   public static void main(String[] args){
+
+    String inputWord = readWord();
+    translateWord(inputWord);
+
+  }
+
+  public static String readWord() {
     Scanner scan = new Scanner(System.in);
     System.out.println("Write a word that you want to translate:");
 
     String inputWord = scan.next();
+    return inputWord;
+  }
 
+  public static void translateWord(String inputWord){
     System.out.println("The word in pig latin is: ashtray ");
   }
 }
