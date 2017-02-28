0ca4da1273d367b5b4c31d0fcc769d9ce08f2d61
kribe48 <kribe606@gmail.com>
2017-02-28 08:54:12 +0100

    added functionality for translating a word

--- src/main/java/piggymcpigface/PiggyMcPigface.java
+++ src/main/java/piggymcpigface/PiggyMcPigface.java
@@ -13,7 +13,29 @@ public class PiggyMcPigface {
   }
 
   public static String translateWord(String inputWord){
-
-    return "ashtray";
+    String pigLatin = "";
+    int wordLength = inputWord.length();
+    int vowelIdx = indexOfFirstVowel(inputWord);
+    if (vowelIdx == 0){
+      pigLatin = inputWord.substring(1,wordLength-1) + inputWord.substring(0,0) + "way";
+    }
+    else if(vowelIdx > 0){
+      pigLatin = inputWord.substring(vowelIdx,wordLength-1)  + inputWord.substring(0,vowelIdx) + "ay";
+    }
+    return pigLatin;
   }
+
+  public static int indexOfFirstVowel(String word){
+    String loweredWord = word.toLowerCase();
+    String vowels = "aeiouy";
+    for (int index = 0; index < loweredWord.length(); index++){
+
+        if (vowels.contains(String.valueOf(loweredWord.charAt(index)))){
+            return index;
+        }
+    }
+    // handle cases where a vowel is not found
+    return -1;
+}
+
 }
