2b5d8d32352a76c83183a8087b49a1e69e6b823f
perbostrm <per.bostrom@liu.se>
2017-02-28 09:06:15 +0100

    improved tests for vowel detection and word translation

--- src/test/java/tests/TestyMcTestface.java
+++ src/test/java/tests/TestyMcTestface.java
@@ -8,16 +8,38 @@ import piggymcpigface.*;
 
 public class TestyMcTestface {
   @Test
-  public void translatedWordShouldBeAshtray() {
+  public void translateWordBeginningWithConsonant() {
     PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
-    assertEquals("ashtray", piggyMcPigface.translateWord("hej"));
+    assertEquals("ananabay", piggyMcPigface.translateWord("banana"));
+  };
+  @Test
+  public void translateWordBeginningWithMultipleConsonants() {
+    PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
+    assertEquals("eerschay", piggyMcPigface.translateWord("cheers"));
   }
   @Test
-  public void findingFirstVowel() {
+  public void translateWordBeginningWithVowel() {
+    PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
+    assertEquals("omeletway", piggyMcPigface.translateWord("omelet"));
+  }
+  @Test
+  public void findingFirstVowelNo() {
     PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
     assertEquals(-1, piggyMcPigface.indexOfFirstVowel("nvwl"));
+  }
+  @Test
+  public void findingFirstVowelFirst() {
+    PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
     assertEquals(0, piggyMcPigface.indexOfFirstVowel("avowelfirst"));
+  }
+  @Test
+  public void findingFirstVowelMiddle() {
+    PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
     assertEquals(4, piggyMcPigface.indexOfFirstVowel("vwlmiddle"));
+  }
+  @Test
+  public void findingFirstVowelLast() {
+    PiggyMcPigface piggyMcPigface = new PiggyMcPigface();
     assertEquals(6, piggyMcPigface.indexOfFirstVowel("vwllsta"));
   }
 
